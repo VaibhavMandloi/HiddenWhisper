5f9c16d127a9fc5b9f553d9013c619f464ab4cc0
VaibhavMandloi <vaibhavmandloi2208@gmail.com>
2025-08-10 16:26:09 +0530

    add message card

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -141,7 +141,7 @@ export default function SignUpForm() {
                 <FormItem>
                   <FormLabel>Email</FormLabel>
                   <Input {...field} name="email" />
-                  <p className='text-muted text-gray-400 text-sm'>We will send you a verification code</p>
+                  <p className=' text-gray-400 text-sm'>We will send you a verification code</p>
                   <FormMessage />
                 </FormItem>
               )}
